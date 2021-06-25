b4dbc33911bbd52404d304ee74552d5668d9c385
Shidiq <shidiq.hidayat@gmail.com>
2021-06-25 20:37:05 +0700

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# PySync
+
+> Python-Syncronizer version 0.0.1
+
+## Requirements
+
+- tqdm
+
+## How to use
+
+- Edit destination path in `target`
+- Edit source path in `sourcepath`
+- Run `python main.py`
+
+> See log-activity in `target/last_sync.txt`
---
+++ main.py
@@ -0,0 +1,53 @@
+'''
+PySync v.0.0.1
+
+by: Shidiq Nur Hidayat (2021)
+'''
+
+import os
+import shutil
+from tqdm import tqdm
+import datetime
+
+print("\n==================================")
+print(f"          PySync v.0.0.1           ")
+print("==================================\n")
+
+# source
+sourcepath = "/Users/shidiq/GeNose"
+
+# destination
+target = "/Volumes/99% VIRUS"
+
+base = os.path.basename(sourcepath)
+path_ = os.path.abspath(sourcepath)
+folders = [os.path.abspath(f) for f, _, _ in os.walk(path_)]
+icopy = 0
+for folder in tqdm(folders, total=len(folders), desc='Scanning folders: '):
+    list_ = [f for f in os.listdir(folder)]
+
+    for i, item in enumerate(list_):
+        try:
+            fullpath = os.path.join(folder, item)
+            if os.path.isfile(fullpath):
+                despath = os.path.join(target, fullpath[fullpath.find(base):])
+                if os.path.isfile(despath):
+                    if os.stat(fullpath).st_mtime - os.stat(despath).st_mtime > 1:
+                        shutil.copy2(fullpath, despath)
+                        icopy += 1
+                else:
+                    os.makedirs(os.path.dirname(despath), exist_ok=True)
+                    shutil.copy2(fullpath, despath)
+                    icopy += 1
+        except Exception as ex:
+            print(str(ex))
+            print(f"File: {item}")
+
+
+f = open(os.path.join(target, os.path.basename(path_), 'last_sync.txt'), 'a')
+f.write(f"Last sync: {datetime.datetime.now()}\n")
+f.close()
+
+print("\n==================================")
+print(f"Sync update: {icopy} files!")
+print("==================================\n")
